ac010353b411ce3f0e9a1f762067cee362404d4d
Tao Yue <ziggytao@163.com>
2020-02-04 15:28:54 +0800

    Update PAT A1002.c

--- PAT A1002.c
+++ PAT A1002.c
@@ -3,39 +3,7 @@ This time, you are supposed to find A+B where A and B are two polynomials.
 Input Specification:
 Each input file contains one test case. Each case occupies 2 lines, and each line contains the information of a polynomial:
 
-K N
-​1
-​​  a
-​N
-​1
-​​ 
-​​  N
-​2
-​​  a
-​N
-​2
-​​ 
-​​  ... N
-​K
-​​  a
-​N
-​K
-​​ 
-​​ 
 
-where K is the number of nonzero terms in the polynomial, N
-​i
-​​  and a
-​N
-​i
-​​ 
-​​  (i=1,2,⋯,K) are the exponents and coefficients, respectively. It is given that 1≤K≤10，0≤N
-​K
-​​ <⋯<N
-​2
-​​ <N
-​1
-​​ ≤1000.
 
 Output Specification:
 For each test case you should output the sum of A and B in one line, with the same format as the input. Notice that there must be NO extra space at the end of each line. Please be accurate to 1 decimal place.
@@ -48,6 +16,7 @@ Sample Input:
     
 Sample Output:
 3 2 1.5 1 2.9 0 3.2
+
 
 #include <stdio.h>
 int main() {
